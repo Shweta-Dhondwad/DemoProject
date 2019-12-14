28d54469e8e4d2e5f081d9f18e091af43e46bdcd
Shweta-Dhondwad <dhondvadshweta08@gmail.com>
2019-12-13 22:04:34 -0800

    dddd

--- src/DemoProject/DemoProject.java
+++ src/DemoProject/DemoProject.java
@@ -2,12 +2,12 @@ package DemoProject;
 
 public class DemoProject {
 	static int x=5;
-
+static int y=10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("x=" +x);
 
-System.out.println("x=" +x);
+System.out.println("y=" +y);
 
 	}
 
