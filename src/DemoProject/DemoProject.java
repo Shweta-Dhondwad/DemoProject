c2a3f95b5f823cab247b108396f187f2586d4dcc
Shweta-Dhondwad <dhondvadshweta08@gmail.com>
2019-12-13 21:51:46 -0800

    First commit

---
+++ src/DemoProject/DemoProject.java
@@ -0,0 +1,11 @@
+package DemoProject;
+
+public class DemoProject {
+	static int x=5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("x=" +x);
+	}
+
+}
